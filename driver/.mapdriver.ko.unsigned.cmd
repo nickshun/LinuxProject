079cc276e4e26a7227bc1da4d1b592e598d923ff
nickshun <eric.nicholson@mymail.champlain.edu>
2015-11-15 17:56:45 -0500

    Delete .mapdriver.ko.unsigned.cmd

--- driver/.mapdriver.ko.unsigned.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/eric/project/LinuxProject/driver/mapdriver.ko.unsigned := ld -r -m elf_x86_64 -T /usr/src/kernels/2.6.32-573.7.1.el6.x86_64/scripts/module-common.lds --build-id -o /home/eric/project/LinuxProject/driver/mapdriver.ko.unsigned /home/eric/project/LinuxProject/driver/mapdriver.o /home/eric/project/LinuxProject/driver/mapdriver.mod.o 
