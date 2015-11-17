bc7ede63cbe29dfc56b66c43e20623523c24313b
nickshun <eric.nicholson@mymail.champlain.edu>
2015-11-17 17:33:57 -0500

    got forked

--- forking.c
+++ forking.c
@@ -1,15 +1,15 @@
-/*#include <unistd.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main(void)
 {
-    	char *argv[] = { "/project/sh", "-c", "genmap",  0};
-    	char *envp[] =
+    	char *argv[] = {"/bin/sh", "genmap.sh", 0 };
+   	char *envp[] =
     	{
-        	"HOME=/",
-        	"PATH=/project:/project/LinuxProject",
-        	0
-    	};
+       		 "PATH=/bin",
+       	 	0
+	};
+
 	int pid = fork();
 	if (pid == 0)
 	{
@@ -26,21 +26,4 @@ int main(void)
     		return -1;
 	}
 	return 0;
-}*/
-
-#include <unistd.h>
-#include <stdio.h>
-
-int main(void)
-{
-    char *argv[] = {"/genmap.sh",  "sh", 0 };
-    char *envp[] =
-    {
-        "USER=eric",
-	"PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/eric/bin",
-        0
-    };
-    execve(argv[0], argv, envp);
-	fprintf(stderr, "Oops!\n");
-    return -1;
 }
