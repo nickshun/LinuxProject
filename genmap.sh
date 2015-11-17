3c74084a984a547d85120419009fa6c7708fb7ee
nickshun <eric.nicholson@mymail.champlain.edu>
2015-11-17 16:20:56 -0500

    updated stuff

--- genmap.sh
+++ genmap.sh
@@ -2,9 +2,10 @@
 
 genmap(){
 
-local width height i j carac charArray randNum
+local width height i j carac charArray randNum blanks half
 
 let carac='0'
+let blanks=0
 
 if [ -z "$1" ] 
 then
@@ -18,12 +19,22 @@ else
         width=$1
         height=$2
 fi
+let half=width/2
 	for((i = 1; i<= width; i++))
 	do
+		let blanks=0
 		for((j=1; j<= height; j++))
 		do
 			#Get random num for ASCII
-			let randNum=$((RANDOM%90+33))
+			let randNum=$((RANDOM%90+32))
+			if [ $randNum -eq 32 ]
+			then
+				let "blanks++"
+			fi
+			if [ "$blanks" -gt "$half" ]
+			then
+				printf "STDERR too many blanks"
+			fi
 			carac=\\$(printf '%03o' $(($randNum)))
 			printf "\e[$i;${j}f\e[1;34m${carac}\e[0m";
 		done
