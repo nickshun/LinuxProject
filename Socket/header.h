3647cc6c01e7f51e0e46a3a05adfb9faa6b5a4d8
nickshun <eric.nicholson@mymail.champlain.edu>
2015-11-30 12:27:58 -0500

    Added Andy's socket stuff

---
+++ Socket/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+long defaultPort = 23032;
+string defaultIP = "127.0.0.1";
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
